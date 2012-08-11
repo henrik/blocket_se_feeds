a97fc72fe2614714c7a664abacb6d670e63477fe
Henrik N <henrik@nyh.se>
2012-08-11 09:28:50 +0200

    Move Rack::Cache to config.ru.

--- app.rb
+++ app.rb
@@ -4,15 +4,6 @@ require "rubygems"
 require "bundler"
 Bundler.require :default, (ENV['RACK_ENV'] || "development").to_sym
 
-# Defined in ENV on Heroku. To try locally, start memcached and uncomment:
-# ENV['MEMCACHE_SERVERS'] = "localhost"  # DO NOT COMMIT
-if memcache_servers = ENV['MEMCACHE_SERVERS']
-  use Rack::Cache,
-    verbose: true,
-    metastore:   "memcached://#{memcache_servers}",
-    entitystore: "memcached://#{memcache_servers}"
-end
-
 require "./rack_cache_patch"
 require "./blocket"
 
--- config.ru
+++ config.ru
@@ -1,2 +1,12 @@
 require "./app"
+
+# Defined in ENV on Heroku. To try locally, start memcached and uncomment:
+# ENV['MEMCACHE_SERVERS'] = "localhost"  # DO NOT COMMIT
+if memcache_servers = ENV['MEMCACHE_SERVERS']
+  use Rack::Cache,
+    verbose: true,
+    metastore:   "memcached://#{memcache_servers}",
+    entitystore: "memcached://#{memcache_servers}"
+end
+
 run Sinatra::Application

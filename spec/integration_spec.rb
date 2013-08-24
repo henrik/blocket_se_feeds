cde6b8d15e1cbea330d1f9a11c27c28348342399
Henrik N <henrik@nyh.se>
2013-08-24 11:36:52 +0200

    Integration spec for search/feed.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,12 +5,22 @@ require_relative "../app"
 describe "The app" do
   include Rack::Test::Methods
 
-  describe "/" do
+  describe "start page" do
     before { get "/" }
 
     it "works" do
       last_response.should be_ok
       last_response.body.should include "<h1>Blocket"
+    end
+  end
+
+  describe "query" do
+    before { get "/stockholm?q=fisk" }
+
+    it "works" do
+      last_response.should be_ok
+      last_response.headers["Content-Type"].should include "application/atom+xml"
+      last_response.body.should include "<entry>"
     end
   end
 

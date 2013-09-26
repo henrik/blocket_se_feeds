5c89210986b6984313dd3239ed4fb3240230f8ad
Henrik Nyh <henrik@nyh.se>
2013-09-26 05:56:52 +0200

    Cleanup.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ require "./blocket"
 
 set :views, -> { root }
 
-get '/' do
+get "/" do
   @title = "Blocket.se RSS-feed"
   slim :index
 end
@@ -17,29 +17,32 @@ end
 get %r{/(.+)} do
   url = "http://www.blocket.se#{request.fullpath}"
   content_type "application/atom+xml", charset: "utf-8"
-  begin
-    heroku_timeout do
-      body = Blocket::ScraperFeeder.new(url).to_atom
-      # Only if body didn't time out. Don't cache errors.
-      unless params[:nocache]
-        cache_control :public, max_age: 1800  # 30 mins.
-      end
-      body
-    end
-  rescue Blocket::Scraper::PageNotFoundError
-    halt 404, "No such page."
-  rescue Timeout::Error
-    halt 504, "Timeout."
-  rescue => e
-    # debug issue
-    if e.message.include?("Mechanize::Image")
-      debug_e = StandardError.new("#{e.class} #{e.message} [ PATH #{request.fullpath} ]")
-      e = debug_e
-    end
 
-    track_exception(e)
-    Blocket::ScraperFeeder.render_exception(e)
+  heroku_timeout do
+    body = Blocket::ScraperFeeder.new(url).to_atom
+    # Only if body didn't time out. Don't cache errors.
+    unless params[:nocache]
+      cache_control :public, max_age: 1800  # 30 mins.
+    end
+    body
   end
+rescue Blocket::Scraper::PageNotFoundError
+  halt 404, "No such page."
+rescue Timeout::Error
+  halt 504, "Timeout."
+rescue => e
+  render_exception(e)
+end
+
+def render_exception(e)
+  # debug issue
+  if e.message.include?("Mechanize::Image")
+    debug_e = StandardError.new("#{e.class} #{e.message} [ PATH #{request.fullpath} ]")
+    e = debug_e
+  end
+
+  track_exception(e)
+  Blocket::ScraperFeeder.render_exception(e)
 end
 
 def track_exception(e)
